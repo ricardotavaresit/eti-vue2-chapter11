be12661f5c67cc4ec4575288daf4a2230c0d024d
rtavares <ricardotavaresit@gmail.com>
2022-06-07 20:25:30 +0100

    create route to not found route

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 import HomeComponent from "./components/HomeComponent";
 import ProductComponent from "./components/ProductComponent";
 import TasksComponent from "./components/TasksComponent";
+import NotFountComponent from "./components/NotFountComponent";
 
 export default [
   { path: '/', component: HomeComponent },
   { path: '/products', component: ProductComponent, name: 'route_products' },
   { path: '/task/:id', component: TasksComponent, name: "task", props: true },
-  { path: '/redirect', redirect: '/products' }
+  { path: '/redirect', redirect: '/products' },
+  { path: '*', component: NotFountComponent}
 ]
