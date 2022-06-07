46bfb4b63db1f6e78a3c9f030bd5b3cb7bd359c6
rtavares <ricardotavaresit@gmail.com>
2022-06-07 19:58:58 +0100

    rename route

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,6 @@ import TasksComponent from "./components/TasksComponent";
 
 export default [
   { path: '/', component: HomeComponent },
-  { path: '/products', component: ProductComponent },
+  { path: '/products', component: ProductComponent, name:'route_products' },
   { path: '/tasks', component: TasksComponent }
 ]
