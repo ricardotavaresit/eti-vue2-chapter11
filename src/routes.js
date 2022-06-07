7afacf9a9ba37b04e424532cfeb2270a32bd638d
rtavares <ricardotavaresit@gmail.com>
2022-06-07 20:18:21 +0100

    create redirect route

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import TasksComponent from "./components/TasksComponent";
 
 export default [
   { path: '/', component: HomeComponent },
-  { path: '/products', component: ProductComponent, name:'route_products' },
-  { path: '/task/:id', component: TasksComponent, name:"task", props: true}
+  { path: '/products', component: ProductComponent, name: 'route_products' },
+  { path: '/task/:id', component: TasksComponent, name: "task", props: true },
+  { path: '/redirect', redirect: '/products' }
 ]
